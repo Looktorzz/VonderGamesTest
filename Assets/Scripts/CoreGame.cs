9b07d296469e6525d090dcde26741dda77678c52
Jiffy - Kulisara <kulisutt.j@gmail.com>
2024-09-30 01:25:58 +0700

    Make Core game to singleton

--- Assets/Scripts/CoreGame.cs
+++ Assets/Scripts/CoreGame.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class CoreGame : MonoBehaviour
+public class CoreGame : Singleton<CoreGame>
 {
+    [SerializeField]
+    private ItemDataConfig _itemDataConfig;
+    public ItemDataConfig ItemDataConfig => _itemDataConfig;
+
+    [SerializeField]
+    private InventoryManager _inventoryManager;
+    public InventoryManager InventoryManager => _inventoryManager;
+
     [SerializeField]
     private TimeHopController _timeHopController;
 
     [SerializeField]
     private TimeHopUI _timeHopUI;
 
-    private void Awake()
+    private void Start()
     {
         _timeHopController.Init();
         _timeHopUI.Setup();
